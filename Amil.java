8a24e9f6963d19f9bc7fff7f9d951a2fe487167b
FikriFikrulMubarok <fikrifikrulmubarok@y7mail.com>
2014-09-15 16:59:40 +0700

    fikri fikrul praktikum rpll

---
+++ Amil.java
@@ -0,0 +1,16 @@
+public class Amil{
+		String[][] Amil = {{"01", "Akang Surakang"}
+				   ,{"02", "Usro Kurniawan"}
+				   ,{"03", "Nurwahidin"}};
+	String rt, Nama, Pos, Ptgas;
+	int ZPJ, Shdq, Tot;
+	public InfoAmil(){
+		System.out.println("RT = " +rt);
+		System.out.println("Nama = " +Nama);
+		System.out.println("Posisi = " +Pos);
+		System.out.println("Zakat yang dibayar = " +ZPJ);
+		System.out.println("Shodaqoh = " +Shdq);
+		System.out.println("Total yang di bayar = " +Tot);
+		System.out.println("Petugas = " +Ptgas);
+	}
+}
